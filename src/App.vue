66a7d9034b8ea2e104cce9b60522d4bce23747ed
aurelie <ferre.aurelie@wanadoo.fr>
2025-04-13 16:18:53 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script setup>
+
+import {onMounted, ref} from "vue";
+
+const quiz = ref(null)
+onMounted(() => {
+
+})
+</script>
+
+<style>
+.container {
+  margin-top: 2rem;
+}
+</style>
